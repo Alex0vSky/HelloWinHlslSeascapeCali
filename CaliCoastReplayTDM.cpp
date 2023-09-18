dc1949fda62c806ad8ece8a153a54d6c7d3075bf
Alex0vSky <52796897+Alex0vSky@users.noreply.github.com>
2023-09-19 00:59:35 +0300

    fix

--- CaliCoastReplayTDM.cpp
+++ CaliCoastReplayTDM.cpp
@@ -3,11 +3,69 @@
 #include "stdafx.h"
 using namespace prj_3d::HelloWinHlsl;
 using namespace prj_3d::HelloWinHlsl::ClientTy;
+using CurDxVer = DxVer::v9;
 //using CurDxVer = DxVer::v10;
-using CurDxVer = DxVer::v12;
+//using CurDxVer = DxVer::v11;
+//using CurDxVer = DxVer::v12;
 
 template<class TSPE, class T = TSPE> class CaliCoastReplayTDM; // primary template
 
+template<> class CaliCoastReplayTDM<DxVer::v9> : public CurClientApp<DxVer::v9> { using T = DxVer::v9;
+	CPtr< IDirect3DVertexShader9 > m_pcVs;
+	CPtr< IDirect3DPixelShader9 > m_pcPs;
+	CPtr< IDirect3DVertexBuffer9 > m_pcVertexBuf;
+	D3DPRIMITIVETYPE m_enuPrimitiveType;
+	UINT m_uStartVertex, m_uPrimitiveCount;
+	LikeDxut::Timer m_oTimer;
+
+	struct PS_CONST_BUFF { 
+		D3DXVECTOR4 iResolution;
+		float iTime;
+	};
+	PS_CONST_BUFF PsConstData = { };
+	Dx::Tool::Shader::ConstantSetterDx9::Setter::uptrc_t m_puPsConstSetter;
+
+	bool init(DxCtx<T>::cref_ptr_t crpustDxCtx, ToolCtx<T>::cref_ptr_t puoTools, Adjust<T>* poAdjustDxAux) {
+		Sys::Hr hr;
+
+		// Load shaders
+		m_pcVs = puoTools ->shader( ) ->loader( ) ->byteCode( ) ->fromFile( ) ->Vs( L"Cali_vs_Dx9.cso" );
+		m_pcPs = puoTools ->shader( ) ->loader( ) ->byteCode( ) ->fromFile( ) ->Ps( L"Cali_ps_Dx9.cso" );
+		if ( !m_pcVs || !m_pcPs )
+			return false;
+		hr = crpustDxCtx ->m_pcD3dDevice ->SetVertexShader( m_pcVs.Get( ) );
+		hr = crpustDxCtx ->m_pcD3dDevice ->SetPixelShader( m_pcPs.Get( ) );
+
+		// Create and set vertex buffer
+		auto stBufV = puoTools ->quad( ) ->createVertexBuf( );
+		if ( !stBufV )
+			return false;
+		m_pcVertexBuf = stBufV ->m_stVertexBuf.m_pcBuffer;
+		m_enuPrimitiveType = stBufV ->m_enuPrimitiveType;
+		m_uStartVertex = stBufV ->m_uStartVertex;
+		m_uPrimitiveCount = stBufV ->m_uPrimitiveCount;
+		crpustDxCtx ->m_pcD3dDevice ->SetStreamSource( 0, m_pcVertexBuf.Get( ), 0, stBufV ->m_uStride );
+		crpustDxCtx ->m_pcD3dDevice ->SetFVF( stBufV ->m_stVertexBuf.m_dwFVF );
+
+		// Shader constants
+		m_puPsConstSetter = puoTools ->shaderConstSetterDx9( ) ->create( m_pcPs );
+		if ( !m_puPsConstSetter )
+			return false;
+		auto stPp = Dx::Descr<T>::presentParameters( crpustDxCtx );
+		bool bRv = m_puPsConstSetter ->setFloat4( "iResolution", D3DXVECTOR4{ (float)stPp.BackBufferWidth, (float)stPp.BackBufferHeight, 0, 0 } );
+
+		return true;
+	}
+
+	void render_frame(DxCtx<T>::cref_ptr_t crpustDxCtx, Dynamic<T>::cref_ptr_t) {
+        Sys::Hr hr;
+		m_puPsConstSetter ->setFloat( "iTime", m_oTimer.get( ) );
+
+		hr = crpustDxCtx ->m_pcD3dDevice ->BeginScene( );
+		hr = crpustDxCtx ->m_pcD3dDevice ->DrawPrimitive( m_enuPrimitiveType, m_uStartVertex, m_uPrimitiveCount );
+		crpustDxCtx ->m_pcD3dDevice ->EndScene();
+	}
+};
 template<> class CaliCoastReplayTDM<DxVer::v10> : public CurClientApp<DxVer::v10> { using T = DxVer::v10;
 	CPtr< ID3D10InputLayout> m_pcLayout;
 	CPtr< ID3D10Buffer > m_pcVertexBuf;
@@ -16,8 +74,8 @@ template<> class CaliCoastReplayTDM<DxVer::v10> : public CurClientApp<DxVer::v10
 	LikeDxut::Timer m_oTimer;
 
 	struct PS_CONST_BUFF { 
-		D3DXVECTOR4 ve2Resolution;
-		float fTime;
+		D3DXVECTOR4 iResolution;
+		float iTime;
 	};
 	PS_CONST_BUFF PsConstData = { };
 	Dx::Tool::ConstantBuf::viaStruct::Accessor< T, PS_CONST_BUFF >::uptr_t m_puoConstBufAccessor;
@@ -41,22 +99,22 @@ template<> class CaliCoastReplayTDM<DxVer::v10> : public CurClientApp<DxVer::v10
 		crpustDxCtx ->m_pcD3dDevice ->IASetInputLayout( m_pcLayout.Get( ) );
 
 		// Create and set vertex buffer
-		auto stBuf = puoTools ->quad( ) ->createVertexBuf();
-		if ( !stBuf )
+		auto stVBuf = puoTools ->quad( ) ->createVertexBuf();
+		if ( !stVBuf )
 			return false;
-		m_pcVertexBuf = stBuf ->m_stVertexBuf.m_pcBuffer;
+		m_pcVertexBuf = stVBuf ->m_stVertexBuf.m_pcBuffer;
 		crpustDxCtx ->m_pcD3dDevice ->IASetVertexBuffers( 
-			0, stBuf ->m_stVertexBuf.c_uNumBuffers, &stBuf->m_stVertexBuf.m_veBuffers[ 0 ]
-			, &stBuf->m_stVertexBuf.m_veStride[ 0 ], &stBuf->m_stVertexBuf.m_veOffset[ 0 ] );
+			0, stVBuf ->m_stVertexBuf.c_uNumBuffers, &stVBuf->m_stVertexBuf.m_veBuffers[ 0 ]
+			, &stVBuf->m_stVertexBuf.m_veStride[ 0 ], &stVBuf->m_stVertexBuf.m_veOffset[ 0 ] );
 
 		// Input assembly
-		crpustDxCtx ->m_pcD3dDevice ->IASetPrimitiveTopology( stBuf ->m_enuTopology );
+		crpustDxCtx ->m_pcD3dDevice ->IASetPrimitiveTopology( stVBuf ->m_enuTopology );
 
 		// Supply the pixel shader constant data
 		PS_CONST_BUFF PsConstData = { };
 		DXGI_SWAP_CHAIN_DESC stPp = Dx::Descr<T>::swapChain( crpustDxCtx );
-		PsConstData.ve2Resolution.x = (float)stPp.BufferDesc.Width;
-		PsConstData.ve2Resolution.y = (float)stPp.BufferDesc.Height;
+		PsConstData.iResolution.x = (float)stPp.BufferDesc.Width;
+		PsConstData.iResolution.y = (float)stPp.BufferDesc.Height;
 		// Create a constant buffer
 		m_puoConstBufAccessor = puoTools ->constBufAccessorFactory( ) ->createAccessor( PsConstData );
 		auto stCBuf = m_puoConstBufAccessor ->getConstantBuf( );
@@ -71,9 +129,77 @@ template<> class CaliCoastReplayTDM<DxVer::v10> : public CurClientApp<DxVer::v10
 	void render_frame(DxCtx<T>::cref_ptr_t crpustDxCtx, Dynamic<T>::cref_ptr_t) {
 		// Update timer value
 		m_puoConstBufAccessor ->passToShader( [this](PS_CONST_BUFF *p) { 
-				p ->fTime = m_oTimer.get( );
+				p ->iTime = m_oTimer.get( );
 			});
         crpustDxCtx ->m_pcD3dDevice ->Draw( 4, 0 );
+	}
+};
+template<> class CaliCoastReplayTDM<DxVer::v11> : public CurClientApp<DxVer::v11> { using T = DxVer::v11;
+	CPtr< ID3D11VertexShader > m_pcVs;
+	CPtr< ID3D11PixelShader > m_pcPs;
+	CPtr< ID3D11InputLayout> m_pcLayout;
+	CPtr< ID3D11Buffer > m_pcVertexBuf;
+	LikeDxut::Timer m_oTimer;
+
+	struct PS_CONST_BUFF { 
+		D3DXVECTOR4 iResolution;
+		float iTime;
+	};
+	PS_CONST_BUFF PsConstData = { };
+	Dx::Tool::ConstantBuf::viaStruct::Accessor< T, PS_CONST_BUFF >::uptr_t m_puoConstBufAccessor;
+
+	bool init(DxCtx<T>::cref_ptr_t crpustDxCtx, ToolCtx<T>::cref_ptr_t puoTools, Adjust<T>* poAdjustDxAux) {
+		Sys::Hr hr;
+
+		// Load shaders
+		std::vector<BYTE> veShaderByte;
+		m_pcVs = puoTools ->shader( ) ->loader( ) ->byteCode( ) ->fromFile( ) ->Vs( L"Cali_vs_Dx11.cso", &veShaderByte );
+		m_pcPs = puoTools ->shader( ) ->loader( ) ->byteCode( ) ->fromFile( ) ->Ps( L"Cali_ps_Dx11.cso" );
+		if ( !m_pcVs || !m_pcPs )
+			return false;
+		crpustDxCtx ->m_pcDeviceContext ->VSSetShader( m_pcVs.Get( ), nullptr, 0 );
+		crpustDxCtx ->m_pcDeviceContext ->PSSetShader( m_pcPs.Get( ), nullptr, 0 );
+
+		// Set vertex input layout
+		m_pcLayout = puoTools ->quad( ) ->createLayout( veShaderByte );
+		if ( !m_pcLayout )
+			return false;
+		crpustDxCtx ->m_pcDeviceContext ->IASetInputLayout( m_pcLayout.Get( ) );
+
+		// Create and set vertex buffer
+		auto stVBuf = puoTools ->quad( ) ->createVertexBuf();
+		if ( !stVBuf )
+			return false;
+		m_pcVertexBuf = stVBuf ->m_stVertexBuf.m_pcBuffer;
+		crpustDxCtx ->m_pcDeviceContext ->IASetVertexBuffers( 
+			0, stVBuf ->m_stVertexBuf.c_uNumBuffers, &stVBuf->m_stVertexBuf.m_veBuffers[ 0 ]
+			, &stVBuf->m_stVertexBuf.m_veStride[ 0 ], &stVBuf->m_stVertexBuf.m_veOffset[ 0 ] );
+
+		// Input assembly
+		crpustDxCtx ->m_pcDeviceContext ->IASetPrimitiveTopology( stVBuf ->m_enuTopology );
+
+		// Supply the pixel shader constant data
+		PS_CONST_BUFF PsConstData = { };
+		DXGI_SWAP_CHAIN_DESC stPp = Dx::Descr<T>::swapChain( crpustDxCtx );
+		PsConstData.iResolution.x = (float)stPp.BufferDesc.Width;
+		PsConstData.iResolution.y = (float)stPp.BufferDesc.Height;
+		// Create a constant buffer
+		m_puoConstBufAccessor = puoTools ->constBufAccessorFactory( ) ->createAccessor( PsConstData );
+		auto stCBuf = m_puoConstBufAccessor ->getConstantBuf( );
+		if ( !stCBuf )
+			return false;
+		// Bind the constant buffer to the device
+		crpustDxCtx ->m_pcDeviceContext ->PSSetConstantBuffers( 0, stCBuf ->c_uNumBuffers, stCBuf ->m_veConstantBuffers.data( ) );
+
+		return true;
+	}
+
+	void render_frame(DxCtx<T>::cref_ptr_t crpustDxCtx, Dynamic<T>::cref_ptr_t) {
+		// Update timer value
+		m_puoConstBufAccessor ->passToShader( [this](PS_CONST_BUFF *p) { 
+				p ->iTime = m_oTimer.get( );
+			});
+        crpustDxCtx ->m_pcDeviceContext ->Draw( 4, 0 );
 	}
 };
 template<> class CaliCoastReplayTDM <DxVer::v12> : public CurClientApp<DxVer::v12> { using T = DxVer::v12;
